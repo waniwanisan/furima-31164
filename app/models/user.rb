060274af7dfb5ee65608f1d852468abb357561da
waniwanisan <aboutgeneration@gmail.com>
2020-11-05 11:19:28 +0900

    with_optionsによるまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/, message: "Include both letters and numbers" }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "has already been taken" }
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: "Full-width characters"}
-  validates :first_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: "Full-width characters"}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-  validates :birth_date, presence: true
-
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/, message: "Include both letters and numbers" }
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "has already been taken" }
+    validates :nickname
+    validates :last_name, format: { with: /\A[一-龥ぁ-ん]/, message: "Full-width characters"}
+    validates :first_name, format: { with: /\A[一-龥ぁ-ん]/, message: "Full-width characters"}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :birth_date
+  end
 end
