769884737fafbb5c12078941c9e20aabfdd2d733
waniwanisan <aboutgeneration@gmail.com>
2020-11-06 17:33:59 +0900

    DBに保存できた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
+    params.permit(:name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
   end
 end
