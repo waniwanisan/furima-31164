144938ba681c09d2cd55dc0876cf1208e132640b
waniwanisan <aboutgeneration@gmail.com>
2020-11-07 15:25:15 +0900

    db保存確認、テストコードへ

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,37 +6,32 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true  
-  validates :condition_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :postage_payer_id, presence: true
-  validates :handling_time_id, presence: true
   validates :price, presence: true    
   validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
-  validates :prefecture, presence: true, numericality: { other_than: 0 } 
+  validates :prefecture_id, presence: true, numericality: { other_than: 0 } 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to :user, optional: true
-  validates :category, presence: true, numericality: { other_than: 0 } 
+  validates :category_id, presence: true, numericality: { other_than: 0 } 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   belongs_to :user, optional: true
-  validates :condition, presence: true, numericality: { other_than: 0 } 
+  validates :condition_id, presence: true, numericality: { other_than: 0 } 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :postage_payer
   belongs_to :user, optional: true
-  validates :postage_payer, presence: true, numericality: { other_than: 0 } 
+  validates :postage_payer_id, presence: true, numericality: { other_than: 0 } 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :handling_time
   belongs_to :user, optional: true
-  validates :handling_time, presence: true, numericality: { other_than: 0 } 
+  validates :handling_time_id, presence: true, numericality: { other_than: 0 } 
 
 end
