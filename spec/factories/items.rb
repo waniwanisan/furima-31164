eb20ed3ac239de37a5df8227dc39a46b994b3cbe
waniwanisan <aboutgeneration@gmail.com>
2020-11-07 21:51:18 +0900

    テストコード正常系画像のfactorybot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,5 +9,9 @@ FactoryBot.define do
     handling_time_id {1}
     price {Faker::Number.between(from: 300, to: 9999999)}
     association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
