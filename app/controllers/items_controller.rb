b97eab7a7fa566c0acdcd67c064f526edcba49dc
waniwanisan <aboutgeneration@gmail.com>
2020-11-09 21:13:38 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only:[:show, :edit, :update]
+  before_action :set_item, only:[:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -19,7 +19,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-  
   end
 
   def edit
@@ -37,9 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if current_user == @item.user
+    @item.destroy
     redirect_to root_path
+    end
   end
 
   private
